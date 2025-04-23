247ad033c5409c22416757f7fdc912983f3707ad
uday.saha <udaykumarbittu20062013@gmail.com>
2025-04-24 02:38:08 +0530

    getElementsByName() method implemented

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ function changeColor(newColor) {
   const elem = document.getElementById("demo");
   elem.style.color = newColor;
 }
+
+const collection = document.getElementsByName("devices");
+for (let i = 0; i < collection.length; i++) {
+  if (collection[i].type == "checkbox") {
+    collection[i].checked = true;
+  }
+}
